84a09b700b60c3bd03aa3487d4079b5d2e889e2f
shinyoung-park <phewphewsimsim@naver.com>
2017-10-29 23:37:31 +0900

    setting: babel로 ES6를 ES5로 transpile한 후, 파일 하나로 bundle

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'carousel.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
