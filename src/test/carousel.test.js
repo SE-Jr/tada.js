4876465e884af60f69c1cb3c534e116469d3972b
shinyoung-park <phewphewsimsim@naver.com>
2017-11-07 02:35:43 +0900

    env-setting : mocha, chai를 사용한 테스트 추가
    -TC에서 ES6 문법 사용을 위한 babel설정파일(.babelrc) 추가

---
+++ src/test/carousel.test.js
@@ -0,0 +1,12 @@
+import chai from 'chai';
+chai.should();
+
+describe("Carousel Test", ()=>{
+  it("아무것도 안하니까 true여야 한다.", ()=>{
+    //given
+    const isNothing = true;
+    //when
+    //then
+    isNothing.should.be.true;
+  });
+});
