89f82e623ea147a949c509632384eab101e2e5e0
yonggoonoh <nohyg90@gmail.com>
2017-11-13 23:26:44 +0900

    feat: 캐러셀 렌더링 TC 작성

--- src/test/carousel.test.js
+++ src/test/carousel.test.js
@@ -1,12 +1,49 @@
 import chai from 'chai';
+import Carousel from '../scripts/carousel';
+
 chai.should();
 
-describe("Carousel Test", ()=>{
-  it("아무것도 안하니까 true여야 한다.", ()=>{
-    //given
-    const isNothing = true;
-    //when
-    //then
-    isNothing.should.be.true;
-  });
+describe("Carousel 렌더링", () => {
+	let container;
+
+	beforeEach(() => {
+		container = document.createElement('DIV');
+		container.classList.add('se-container');
+
+		let image = document.createElement('IMG');
+		image.src = 'sample.com';
+
+		let child1 = document.createElement('DIV');
+		child1.appendChild(image);
+		container.appendChild(child1);
+
+		let child2 = document.createElement('DIV');
+		child2.appendChild(image);
+		container.appendChild(child2);
+
+		let child3 = document.createElement('DIV');
+		child3.appendChild(image);
+		container.appendChild(child3);
+
+		document.body.appendChild(container);
+	})
+
+	afterEach(() => {
+		document.body.removeChild(container);
+		container = null;
+	})
+
+	it("하나의 캐러셀에는 하나의 요소만이 보여야한다.", () => {
+		// given
+
+		// when
+		new Carousel();
+
+		// then
+		const container = document.getElementsByClassName('se-container');
+		const visibleElements = document.querySelectorAll('.se-container > div.is-on');
+
+		container.length.should.be.eql(1);
+		visibleElements.length.should.be.eql(1);
+	});
 });
