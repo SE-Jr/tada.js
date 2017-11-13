989ab18f53ff93a09b743656334d948b0fd6d412
shinyoung-park <phewphewsimsim@naver.com>
2017-11-14 02:52:25 +0900

    feat : selector를 지정하여 해당 element를 carousel로 만들 수 있다.

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,21 +1,57 @@
 import "../styles/style.scss";
-import { VERSION } from "./config";
+import { PROJECTOR_CLASS, CONTAINER_CLASS, defaultOption } from "./config";
 
-export default class Carousel {
+export default class SlideProjector {
   constructor(option = {}){
-    this.width = 600; //option.width
-    this.height = 400; //option.height
-
-    this.renderCarousel(document.body);
+    this._initProjector(option);
+    this._setProjectorSize(option);
+    this._wrapInContainer();
+    this._render();
   }
 
-  renderCarousel(targetElement){
-    let wrapper = document.createElement("div");
-    wrapper.style.width = this.width;
-    wrapper.style.height = this.height;
-    wrapper.style.border = "1px solid black";
-    wrapper.innerHTML = "CAROUSEL VERSION " + VERSION + " BEGINS (ﾉ´ヮ´)ﾉ*:･ﾟ✧";
+  _initProjector(option){
+    this.projector = document.querySelector(option.selector);
+    this.projector.classList.add(PROJECTOR_CLASS);
+    this.slideCount = this.projector.children.length;
+  }
 
-    targetElement.appendChild(wrapper);
+  _setProjectorSize(option){
+    this.projectorWidth = option.width || defaultOption.width;
+    this.projectorHeight = option.height || defaultOption.height;
+  }
+
+  _wrapInContainer(){
+    const slideContainer = this._generateSlideContainer();
+    this._setSlideWidth(slideContainer);
+    this._fillProjectorWith(slideContainer);
+  }
+
+  _fillProjectorWith(slideContainer){
+    this.projector.innerHTML = "";
+    this.projector.appendChild(slideContainer);
+  }
+
+  _generateSlideContainer(){
+    const slideContainer = document.createElement("div");
+    const slideContainerWidth = (this.projectorWidth * this.slideCount);
+    slideContainer.style.width = slideContainerWidth + "px";
+    slideContainer.style.height = this.projectorHeight + "px";
+    slideContainer.classList.add(CONTAINER_CLASS);
+    slideContainer.innerHTML = this.projector.innerHTML;
+
+    return slideContainer;
+  }
+
+  _setSlideWidth(slideContainer){
+    const slides = slideContainer.querySelectorAll("div");
+    const slideWidth = (100 / this.slideCount) + "%";
+    for (const slide of slides) {
+      slide.style.width = slideWidth;
+    }
+  }
+
+  _render(){
+    this.projector.style.width = this.projectorWidth + "px";
+    this.projector.style.height = this.projectorHeight + "px";
   }
 }
--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,1 +1,8 @@
 export const VERSION = 1.0;
+export const PROJECTOR_CLASS = "slide-projector";
+export const CONTAINER_CLASS = "slider-projector-container";
+
+export const defaultOption = {
+  width: 400,
+  height: 300
+};
