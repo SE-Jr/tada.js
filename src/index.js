cbf3381848ac7870f125dcdce6c50c1975cd5ca4
yonggoonoh <nohyg90@gmail.com>
2017-11-13 22:13:30 +0900

    feat: media query에 따라 해당하는 css 로드

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import "./styles/style.scss";
+import "./styles/carousel.scss";
+import "./styles/carousel-small.scss";
+import "./styles/carousel-large.scss";
 import Carousel from "./scripts/carousel";
 
 new Carousel();
