888f5e1b0ce18b5c1958c408319fb27202c224e0
sososimsim <phewphewsimsim@naver.com>
2017-11-17 03:27:49 +0900

    selector를 포함하는 옵션을 지정하여 projector를 초기화할 수 있다.
    - default option 초기화 방식 수정
    - 유효 여부를 검사하여 에러 메시지 제공
    등 스터디에서의 의견 반영하여 라이브러리 초기화 부분 세팅
    
    #### TODO
    - test runner 적용 이후 TC
    - CSS naming rule
    - 반응형 지원 방식 결정 후 적용(CSS export 방식 포함)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import Carousel from "./scripts/carousel";
+import SlideProjector from "./scripts/carousel";
 
+/*
+* initialize carousel library
+* projector - container - slide
+*/
 (function(){
-  new Carousel();
+  //using class selector
+  new SlideProjector({
+    selector: ".slide-projector-class"
+  });
+
+  //using id selector
+  new SlideProjector({
+    selector: "#slide-projector-id",
+    width: 600,
+    height: 400
+  });
+
+  //error - option required
+  // new SlideProjector();
+
+  //error - selector required
+  // new SlideProjector({});
 })();
