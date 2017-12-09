1071c2e36a40cc6b083726e7b53f6753b0ab29e0
yonggoonoh <nohyg90@gmail.com>
2017-12-09 15:03:48 +0900

    feat: config를 통해 옵션에 타켓 클래스 추가

---
+++ src/scripts/config.js
@@ -0,0 +1,3 @@
+export const DEFAULT_OPTIONS = {
+    CONTAINER_CLASS: 'sp__container'
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,7 @@
+import { DEFAULT_OPTIONS } from './config';
+
 function SlideProjector(options) {
-    this.option = Object.assign({}, options);
+    this.option = Object.assign({}, DEFAULT_OPTIONS, options);
     console.log(this.option);
 };
 
