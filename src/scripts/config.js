d6c3e1e65c69308d7e563db8773340d7fc940055
yonggoonoh <nohyg90@gmail.com>
2017-12-11 22:10:29 +0900

    feat: nav button class 추가 및 nav config 정의

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,3 +2,14 @@ export const DEFAULT_OPTIONS = {
     CONTAINER_CLASS: 'sp__container',
     SHOW_NAVIGATOR: true
 };
+
+export const NAVIGATOR_CONFIG = {
+    PREV_BUTTON: {
+        CLASS: 'sp__button--prev',
+        SHAPE: '&#x2039'
+    },
+    NEXT_BUTTON: {
+        CLASS: 'sp__button--next',
+        SHAPE: '&#x203a'
+    } 
+};
--- src/scripts/navigator.js
+++ src/scripts/navigator.js
@@ -1,16 +1,20 @@
+import { NAVIGATOR_CONFIG } from './config'
 import dom from './util/dom';
 
 class Navigator {
     constructor() {
         this.prevButton = dom.createElement('button');
-        this.prevButton.innerHTML = '&#x2039';
+        this.prevButton.innerHTML = NAVIGATOR_CONFIG.PREV_BUTTON.SHAPE;
+        this.prevButton.className = NAVIGATOR_CONFIG.PREV_BUTTON.CLASS;
         this.nextButton = dom.createElement('button');
-        this.nextButton.innerHTML = '&#x203a;';
+        this.nextButton.innerHTML = NAVIGATOR_CONFIG.NEXT_BUTTON.SHAPE;
+        this.nextButton.className = NAVIGATOR_CONFIG.NEXT_BUTTON.CLASS;
     }
 
     appendTo(element) {
         element.appendChild(this.prevButton);
         element.appendChild(this.nextButton);
+
     }
 }
 
