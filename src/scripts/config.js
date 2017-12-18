62b1e99bcd67ac5a664527f9421af5bbddee1671
helloheesu <hi.heesu820@gmail.com>
2017-12-18 18:50:33 +0900

    feat: attach click event to indicator

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -8,7 +8,8 @@ export const NAVIGATOR_CLASS = '__sp__navigator';
 export const NAVIGATOR_PREV_CLASS = '__sp__navigator__prev';
 export const NAVIGATOR_NEXT_CLASS = '__sp__navigator__next';
 export const INDICATOR_CLASS = '__sp__indicator__container';
-export const INDICATOR_ITEM_CLASS = '__sp__indicator__item __sp__indicator__item--circle';
+export const INDICATOR_ITEM_CLASS = '__sp__indicator__item';
+export const INDICATOR_ITEM_CIRCLE_CLASS = '__sp__indicator__item--circle';
 
 export const NAVIGATOR_PREV_PHRASE = '이전 슬라이드';
 export const NAVIGATOR_NEXT_PHRASE = '다음 슬라이드';
@@ -16,7 +17,7 @@ export const NAVIGATOR_NEXT_PHRASE = '다음 슬라이드';
 export const DEFAULT_OPTIONS = {
   projectorSelector: PROJECTOR_CLASS,
   slideSelector: SLIDE_CLASS,
-  indicator: true,
+  indicator: 'circle',
   navigator: true
 };
 
--- src/scripts/indicator.js
+++ src/scripts/indicator.js
@@ -1,10 +1,21 @@
 import Component from './component';
-import { INDICATOR_CLASS, INDICATOR_ITEM_CLASS } from './config';
+import { INDICATOR_CLASS, INDICATOR_ITEM_CLASS, INDICATOR_ITEM_CIRCLE_CLASS } from './config';
 
 export default class Indicator extends Component {
   constructor(option, controller) {
     super(option);
     this.init(option, controller);
+  }
+
+  _refineOption(option) {
+    super._refineOption(option);
+    if (option.indicator === true) {
+      this.option.indicator = 'circle';
+    }
+
+    if (this.option.indicator === 'circle') {
+      this.option.itemOptionSelector = INDICATOR_ITEM_CIRCLE_CLASS;
+    }
   }
 
   _initContainer() {
@@ -19,9 +30,31 @@ export default class Indicator extends Component {
     const count = this.controller.slideCount;
     let result = '';
     for (let i = 0; i < count; i++) {
-      result += `<li class="${INDICATOR_ITEM_CLASS}"><button>${i+1}</button></li>`;
+      result += `<li class="${INDICATOR_ITEM_CLASS} ${this.option.itemOptionSelector}"><button>${i+1}</button></li>`;
     }
     return result;
   }
 
+  _attachEvent() {
+    this.container.addEventListener('click', this._moveTo.bind(this));
+  }
+
+  _moveTo(e) {
+    const itemClassSelector = `.${INDICATOR_ITEM_CLASS}`;
+    const item = e.target.closest(itemClassSelector);
+    if (!item) {
+      return;
+    }
+
+    const destSlide = this._getIndexOf(item);
+    this.controller.moveTo(destSlide);
+  }
+
+  _getIndexOf(item) {
+    let index = 0;
+    for (let currentItem = item; (currentItem = currentItem.previousSibling) != null;) {
+      index++;
+    }
+    return index;
+  }
 }
