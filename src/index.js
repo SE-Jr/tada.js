3f13d6f82ff395ae7a0272d3e71b9e864417cee7
mojosoeun <mojosoeun@gmail.com>
2018-01-02 23:24:09 +0900

    feat: carousel 라이브러리 구조 변경

--- src/index.js
+++ src/index.js
@@ -1,25 +1,70 @@
-import SlideProjector from './scripts/carousel';
+import './styles/style.scss';
+import { PROJECTOR_CLASS, CONTAINER_CLASS, DEFAULT_OPTIONS, ERROR_MESSAGE } from './scripts/config';
 
-/*
-* initialize carousel library
-* projector - container - slide
-*/
-(function () {
-  // using class selector
-  new SlideProjector({
-    selector: '.slide-projector-class',
-  });
+class SlideProjector {
+  constructor(option) {
+    this.init(option);
+    this.initProjector();
+    this.setProjectorSize();
+    this.wrapInContainer();
+    this.render();
+  }
 
-  // using id selector
-  new SlideProjector({
-    selector: '#slide-projector-id',
-    width: 600,
-    height: 400,
-  });
+  init(option) {
+    if (!option || typeof option !== 'object') {
+      throw new Error(ERROR_MESSAGE.OPTION_REQUIRED);
+    }
+    if (!option.selector || typeof option.selector !== 'string') {
+      throw new Error(ERROR_MESSAGE.INVALID_SELECTOR);
+    }
+    this.option = Object.assign(DEFAULT_OPTIONS, option);
+  }
 
-  // error - option required
-  // new SlideProjector();
+  initProjector() {
+    this.projector = document.querySelector(this.option.selector);
+    this.projector.classList.add(PROJECTOR_CLASS);
+    this.slideCount = this.projector.children.length;
+  }
 
-  // error - selector required
-  // new SlideProjector({});
-}());
+  setProjectorSize() {
+    this.projectorWidth = this.option.width;
+    this.projectorHeight = this.option.height;
+  }
+
+  wrapInContainer() {
+    const slideContainer = this.generateSlideContainer();
+    this.setSlideWidth(slideContainer);
+    this.fillProjectorWith(slideContainer);
+  }
+
+  fillProjectorWith(slideContainer) {
+    this.projector.innerHTML = '';
+    this.projector.appendChild(slideContainer);
+  }
+
+  generateSlideContainer() {
+    const slideContainer = document.createElement('div');
+    const slideContainerWidth = (this.projectorWidth * this.slideCount);
+    slideContainer.style.width = `${slideContainerWidth}px`;
+    slideContainer.style.height = `${this.projectorHeight}px`;
+    slideContainer.classList.add(CONTAINER_CLASS);
+    slideContainer.innerHTML = this.projector.innerHTML;
+
+    return slideContainer;
+  }
+
+  setSlideWidth(slideContainer) {
+    const slides = slideContainer.querySelectorAll('div');
+    const slideWidth = `${100 / this.slideCount}%`;
+    slides.forEach((slide) => {
+      slide.style.width = slideWidth;
+    });
+  }
+
+  render() {
+    this.projector.style.width = `${this.projectorWidth}px`;
+    this.projector.style.height = `${this.projectorHeight}px`;
+  }
+}
+
+window.SP = SlideProjector;
