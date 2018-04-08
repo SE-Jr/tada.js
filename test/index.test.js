3a571975cdb402d608f5c623a96a4cb751e86299
mojosoeun <mojosoeun@gmail.com>
2018-04-08 22:16:24 +0900

    feat: 이벤트 테스트 코드 추가

--- test/index.test.js
+++ test/index.test.js
@@ -2,21 +2,20 @@ import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import Tada from '../src/index';
-
-// chai.should();
 chai.use(sinonChai);
-const expect = chai.expec; // expect ?
+const expect = chai.expect;
 
 
 describe('initial test', function() {
   let sandbox;
   let spyConfig;
   let spyController;
+  let fakeConfig = {fake: 'config'}
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
-    spyConfig = sandbox.stub(Tada.prototype, '_createConfig').callsFake(() => 1);
-    spyController = sandbox.stub(Tada.prototype, '_loadController').callsFake(() => 1);
+    spyConfig = sandbox.stub(Tada.prototype, '_createConfig').callsFake(() => fakeConfig);
+    spyController = sandbox.stub(Tada.prototype, '_loadController').callsFake(() => {});
   });
 
   afterEach(() => {
@@ -32,30 +31,29 @@ describe('initial test', function() {
       //when
       const tada = new Tada(option);
       //then
-      spyConfig.calledWith(option).should.be.true;
+      expect(spyConfig.calledWith(option)).to.be.true;
     });
 
-    //!!!this test case is not working
     it('invoke `_loadController` function', function () {
       //given
-      const config = { a: 'blah' };
       const option = { selector: '.tada-class' };
       //when
       const tada = new Tada(option);
       //then
-      spyController.calledWith(config).should.be.true;
+      expect(spyController.calledWith(fakeConfig)).to.be.true;
     });
   });
 
-  // describe('on >> ', function () {
-  //   it('', function () {
-  //     //given
-  //     const option = { selector: '.tada-class' };
-  //     const tada = new Tada(option);
-  //     //when
-  //     tada.on('next', fun)
-  //
-  //     //then
-  //   });
-  // });
+  describe('on >> ', function () {
+    it('invoke `on` function of controller', function () {
+      const option = { selector: '.tada-class' };
+      const tada = new Tada(option);
+      const nextSpy = sinon.spy();
+      tada.controller = { on: sinon.spy()}
+      //when
+      tada.on('next', nextSpy)
+      //then
+      sinon.assert.calledWith(tada.controller.on, 'next', nextSpy);
+    });
+  });
 });
