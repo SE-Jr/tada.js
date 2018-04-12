f94aa987e63570359426549e883aa32f8c9d620d
mojosoeun <mojosoeun@gmail.com>
2018-04-12 22:33:31 +0900

    feat(-): 사용자 설정 관련 테스트 코드 추가

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@ import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import Tada from '../src/index';
+
 chai.use(sinonChai);
 const expect = chai.expect;
 
@@ -13,7 +14,16 @@ describe('initial test', () => {
   const createFixture = function (){
     fixture = document.createElement('div');
     fixture.id = 'tada-class';
-    fixture.innerHTML = '<div></div>';
+    fixture.innerHTML = `
+        <div>
+            <ul>
+                <li><img src="http://placehold.it/700x500/C2024F/ffffff?text=1" alt="slide-image-01"/></li>
+                <li><img src="http://placehold.it/700x500/04BBBF/ffffff?text=2" alt="slide-image-02"/></li>
+                <li><img src="http://placehold.it/700x500/D2D945/ffffff?text=3" alt="slide-image-03"/></li>
+                <li><img src="http://placehold.it/700x500/FCB13F/ffffff?text=4" alt="slide-image-04"/></li>
+                <li><img src="http://placehold.it/700x500/FF594F/ffffff?text=5" alt="slide-image-05"/></li>
+            </ul>
+        </div>`;
     document.body.append(fixture);
   };
 
@@ -96,4 +106,71 @@ describe('initial test', () => {
       expect(eventSpy.calledWith('next', nextSpy));
     });
   });
+
+  describe('custom Tada ', () => {
+    describe('in default Tada case without no options >> ', () => {
+      it('hide navigator and pagination', () => {
+        //given
+        const option = {selector: '#tada-class'};
+        //when
+        const tada = new Tada(option);
+        //then
+        expect(tada.controller._config.showNavigator).to.be.false;
+        expect(tada.controller._config.showPagination).to.be.false;
+        expect(tada.controller._config.paginationShape).to.equal('circle');
+      });
+    });
+    describe('when user set option >> ', () => {
+      it('can show navigator', () => {
+        //given
+        const option = {selector: '#tada-class', navigator: true };
+        //when
+        const tada = new Tada(option);
+        //then
+        expect(tada.controller._config.showNavigator).to.be.true;
+      });
+      it('can hide navigator', () => {
+        //given
+        const option = {selector: '#tada-class', navigator: false };
+        //when
+        const tada = new Tada(option);
+        //then
+        expect(tada.controller._config.showNavigator).to.be.false;
+      });
+      it('can show pagination', () => {
+        //given
+        const option = {selector: '#tada-class', pagination: true };
+        //when
+        const tada = new Tada(option);
+        //then
+        expect(tada.controller._config.showPagination).to.be.true;
+      });
+      it('can hide pagination', () => {
+        //given
+        const option = {selector: '#tada-class', pagination: false };
+        //when
+        const tada = new Tada(option);
+        //then
+        expect(tada.controller._config.showPagination).to.be.false;
+      });
+      it('can change pagination shape to circle', () => {
+        //given
+        const option = {selector: '#tada-class', paginationShape: 'circle' };
+        //when
+        const tada = new Tada(option);
+        //then
+        expect(tada.controller._config.paginationShape).to.equal('circle');
+      });
+      it('can change pagination shape to bar', () => {
+        //given
+        const option = {selector: '#tada-class', paginationShape: 'bar' };
+        //when
+        const tada = new Tada(option);
+        //then
+        expect(tada.controller._config.paginationShape).to.equal('bar');
+      });
+    });
+  });
+
 });
+
