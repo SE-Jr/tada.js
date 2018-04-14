62ce3d2aa7e76f740e4bce9d8e53316dc58f7916
hyeonmi <>
2018-04-15 00:53:48 +0900

    test: Container테스트 코드 추가

--- test/component/Container.test.js
+++ test/component/Container.test.js
@@ -4,6 +4,7 @@ import sinonChai from 'sinon-chai';
 import testHelper from '../test.helper';
 import Container from '../../src/scripts/component/Container';
 import State from '../../src/scripts/State';
+import Config from '../../src/scripts/Config';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -15,22 +16,54 @@ describe('Container component test >>', () => {
   afterEach(() => {
     testHelper.removeFixture();
   });
-
+  
   describe('slide container rendering >> ', () => {
-    describe('when user set `containerWidth` config >>', () => {
-      it('should equal `containerWidth` config', () => {
-        // given
-        const state = new State();
-        const wrapper = document.getElementById('tada-class');
-        const configs = { wrapper, containerWidth: 100 };
-  
-        // when
-        const container = new Container(configs, state);
-      
-        // then
-        expect(container._containerWidth).to.be.equal(configs.containerWidth);
-      });
+    let state;
+    let wrapper;
+    beforeEach(() => {
+      state = new State();
+      wrapper = document.getElementById('tada-class');
     });
+
+    afterEach(() => {
+      state = null;
+      wrapper = null;
+    });
+
+    it('when create instance of Container should set `currentPage` first slide', () => {
+      // given
+      const option = {};
+      const config = new Config(option, wrapper);
+      
+      // when
+      const container = new Container(config, state);
   
+      // then
+      expect(state.currentPage).to.be.equal(0);
+    });
+
+    it('when user set `containerWidth` config >> should equal `containerWidth` config', () => {
+      // given
+      const option = { containerWidth: 100 };
+      const config = new Config(option, wrapper);
+
+      // when
+      const container = new Container(config, state);
+
+      // then
+      expect(container._containerWidth).to.be.equal(config.containerWidth);
+    });
+
+    it('when user does not set `containerWidth` config >> should equal parent width with `containerWith` config', () => {
+      // given
+      const option = {};
+      const config = new Config(option, wrapper);
+
+      // when
+      const container = new Container(config, state);
+
+      // then
+      expect(container._containerWidth).to.be.equal(wrapper.parentElement.clientWidth);
+    });
   });
 });
