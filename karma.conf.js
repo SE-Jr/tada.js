4d9c80f05108a334a979f7053678a88303142a7d
mojosoeun <mojosoeun@gmail.com>
2018-05-03 22:36:00 +0900

    fix: circle ci

--- karma.conf.js
+++ karma.conf.js
@@ -67,8 +67,32 @@ module.exports = function(config) {
     reporters: ['mocha', 'progress', 'coverage-istanbul'],
 
     coverageIstanbulReporter: {
-      reports: ['text-summary'],
-      fixWebpackSourcePaths: true
+
+      // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+      reports: ['html', 'lcovonly', 'text-summary'],
+
+      // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+      dir: path.join(__dirname, 'coverage'),
+
+      // Combines coverage information from multiple browsers into one report rather than outputting a report
+      // for each browser.
+      combineBrowserReports: true,
+
+      // if using webpack and pre-loaders, work around webpack breaking the source path
+      fixWebpackSourcePaths: true,
+
+      // stop istanbul outputting messages like `File [${filename}] ignored, nothing could be mapped`
+      skipFilesWithNoCoverage: true,
+
+      // Most reporters accept additional config options. You can pass these through the `report-config` option
+      'report-config': {
+
+          // all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
+          html: {
+              // outputs the report in ./coverage/html
+              subdir: 'html'
+          }
+
     },
 
     mochaReporter: {
