123e7c340bf049df6b1275e91752976331933ea3
mojosoeun <mojosoeun@gmail.com>
2018-05-12 23:17:34 +0900

    feat(-) : 빌드 환경 개선
    개발 환경에 sourcemap 적용
    코드 변경시 자동 build 옵션 적용
    로컬, 프로덕트 별 코드 빌드

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+/*
+./src/index.js파일이 너가 일을 시작할 곳이야. 여기에서 필요하다고 하는 것들부터 찾아보렴.
+결과물은 너가 있는 디렉토리(__dirname)에 있는 'dist'라는 폴더 아래에, 'tada.min.js'라는 이름으로 떨궈줘.
+*/
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "tada.min.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "Tada",
+        libraryTarget: "umd",
+        libraryExport: "default",
+    },
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            { // 각 loader에서 사용할 설정들을 세세하게 지정할 수 있어요.
+                test: /\.js$/, // webpack이 '.js'파일을 찾으면 이 로더를 타게 됩니다.
+                exclude: /node_modules/, // 하지만 node_modules 안에 있는거라면 스킵해달라고도 정해주고,
+                loader: "babel-loader", // 이러한 설정으로 사용할 loader는 'babel loader'입니다.
+                options: {
+                    presets: ["es2015"],
+                    plugins: ["transform-class-properties"],
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+};
